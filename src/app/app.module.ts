f11d6e730f6cfe45f7bff4c67530f25e48f3c865
felipesiansi <felipesiansi@gmail.com>
2019-03-17 19:27:31 -0300

    Versão inicial com o angular material devidamente configurado

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { MaterialModule } from './shared/material.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
