6ae97a010099a344365cd2d837e8bc534003f0e8
felipesiansi <felipesiansi@gmail.com>
2019-03-26 06:39:38 -0300

    Criação da toolbar e do sidenav

---
+++ src/app/sales/sales-app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-app',
+  templateUrl: './sales-app.component.html',
+  styleUrls: ['./sales-app.component.scss']
+})
+export class SalesAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
